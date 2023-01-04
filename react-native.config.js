9c032878fc2c8173f37d1dda8f11716de3c91e61
lelebao <494955934@qq.com>
2023-01-04 16:19:01 +0800

    update autolink

--- react-native.config.js
+++ react-native.config.js
@@ -1,21 +1,19 @@
 module.exports = {
   dependency: {
     platforms: {
-      ios: { project: "lib/ios/react-native-amap-geolocation.podspec" },
-      android: { sourceDir: "lib/android" }
-    }
+      android: { sourceDir: "lib/android" },
+    },
   },
   dependencies: {
     "react-native-amap-geolocation": {
       root: __dirname,
       platforms: {
-        ios: { podspecPath: __dirname + "/lib/ios/react-native-amap-geolocation.podspec" },
         android: {
           sourceDir: __dirname + "/lib/android",
           packageImportPath: "import cn.qiuxiang.react.geolocation.AMapGeolocationPackage;",
-          packageInstance: "new AMapGeolocationPackage()"
-        }
-      }
-    }
-  }
+          packageInstance: "new AMapGeolocationPackage()",
+        },
+      },
+    },
+  },
 };
